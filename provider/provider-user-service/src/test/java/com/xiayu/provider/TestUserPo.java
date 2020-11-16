822c720c0f02d2fd72a280b8f3467f09d138c9c8
xiayu <xiayuxuxu@qq.com>
2020-11-16 17:53:23 +0800

    测试环境配置

--- provider/provider-user-service/src/test/java/com/xiayu/provider/TestUserPo.java
+++ provider/provider-user-service/src/test/java/com/xiayu/provider/TestUserPo.java
@@ -1,42 +1,42 @@
-package com.xiayu.provider;
-
-import com.xiayu.provider.api.UserService;
-import com.xiayu.provider.domain.UserPo;
-import com.xiayu.provider.params.UserInsertVo;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-/**
- * Description:
- *
- * @version v1.0.0
- * @Author xiayu
- * @Date 2020/10/3 12:02
- */
-@SpringBootTest
-@RunWith(SpringRunner.class)
-public class TestUserPo {
-    @Autowired
-    private UserService userService;
-
-    @Test
-    public void selectUserByName(){
-        UserPo userPo = userService.selectUserByName("user");
-        System.out.println(userPo.toString());
-    }
-
-    @Test
-    public void insertUser()  {
-        UserInsertVo userInsertVo = new UserInsertVo();
-        userInsertVo.setName("夏雨11111aaa");
-        userInsertVo.setPassword("1234561");
-        userInsertVo.setTel("131643762901");
-        userInsertVo.setType(0);
-        userService.insert(userInsertVo);
-
-    }
-
-}
+//package com.xiayu.provider;
+//
+//import com.xiayu.provider.api.UserService;
+//import com.xiayu.provider.domain.UserPo;
+//import com.xiayu.provider.params.UserInsertVo;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+///**
+// * Description:
+// *
+// * @version v1.0.0
+// * @Author xiayu
+// * @Date 2020/10/3 12:02
+// */
+//@SpringBootTest
+//@RunWith(SpringRunner.class)
+//public class TestUserPo {
+//    @Autowired
+//    private UserService userService;
+//
+//    @Test
+//    public void selectUserByName(){
+//        UserPo userPo = userService.selectUserByName("user");
+//        System.out.println(userPo.toString());
+//    }
+//
+//    @Test
+//    public void insertUser()  {
+//        UserInsertVo userInsertVo = new UserInsertVo();
+//        userInsertVo.setName("夏雨11111aaa");
+//        userInsertVo.setPassword("1234561");
+//        userInsertVo.setTel("131643762901");
+//        userInsertVo.setType(0);
+//        userService.insert(userInsertVo);
+//
+//    }
+//
+//}
--- provider/provider-user-service/src/test/java/com/xiayu/provider/jasypt/Test.java
+++ provider/provider-user-service/src/test/java/com/xiayu/provider/jasypt/Test.java
@@ -1,37 +1,37 @@
-package com.xiayu.provider.jasypt;
-
-import org.jasypt.encryption.StringEncryptor;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.annotation.Resource;
-
-/**
- * Description:
- *
- * @version v1.0.0
- * @Author xiayu
- * @Date 2020/10/7 18:36
- */
-@SpringBootTest
-@RunWith(SpringRunner.class)
-@Rollback
-public class Test {
-    @Resource
-    private StringEncryptor stringEncryptor;
-
-    @org.junit.Test
-    public void possWordCode(){
-        String string1 =stringEncryptor.encrypt("test");
-        System.out.println(string1);
-
-    }
-
-    private String encrypt(String originPassword){
-        String encryptStr = stringEncryptor.encrypt(originPassword);
-        return encryptStr;
-    }
-
-}
+//package com.xiayu.provider.jasypt;
+//
+//import org.jasypt.encryption.StringEncryptor;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.annotation.Rollback;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import javax.annotation.Resource;
+//
+///**
+// * Description:
+// *
+// * @version v1.0.0
+// * @Author xiayu
+// * @Date 2020/10/7 18:36
+// */
+//@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@Rollback
+//public class Test {
+//    @Resource
+//    private StringEncryptor stringEncryptor;
+//
+//    @org.junit.Test
+//    public void possWordCode(){
+//        String string1 =stringEncryptor.encrypt("test");
+//        System.out.println(string1);
+//
+//    }
+//
+//    private String encrypt(String originPassword){
+//        String encryptStr = stringEncryptor.encrypt(originPassword);
+//        return encryptStr;
+//    }
+//
+//}
